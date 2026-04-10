984c3df90e7a09c08fc017b331439c04786f9a5b
leonardocandido-cmyk <leonardo.candido@escola.pr.gov.br>
2026-04-10 12:48:24 +0000

    Crriando navegação no aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/my_home_page.dart';
+import 'package:flutter_app/rotas/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
       ),
-      home: const MyHomePage(title: 'Programação Mobile - Flutter'),
+      initialRoute: '/',
+      routes: appRoutes,
     );
   }
 }
--- lib/menu_lateral.dart
+++ lib/menu_lateral.dart
@@ -13,8 +13,13 @@ class _MenuLateralState extends State<MenuLateral> {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(),
+        children: const [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue
+            ),
+            child: Text('Curso Flutter'),
+          ),
         ],
       ),
     );
