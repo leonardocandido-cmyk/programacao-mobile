9f964e6f1ceda408bc5067d0caf038d42321b2cc
leonardocandido-cmyk <leonardo.candido@escola.pr.gov.br>
2026-04-24 12:53:14 +0000

    Criando os primeiros containers

--- lib/paginas/aula01.dart
+++ lib/paginas/aula01.dart
@@ -9,9 +9,34 @@ class Aula01 extends StatelessWidget {
       appBar: AppBar(
         title: Text("01 - Container"),
       ),
+      // Center centraliza o conteúdo
       body: Center(
-        child: Container(
-          child: Text("Container"),
+        // Column coloca vários elementos como filhos
+        // Organiza os elementos na vertical
+        child: Column(
+          children: [
+            // Container com largura e altura
+            Container(
+              width: double.infinity, // Retorna o valor total disponível
+              height: 100,
+              color: Colors.blue,
+              child: Center(
+                child: Text("Primeiro container"),
+              ),
+            ),
+            // Container com padding e margin
+            Container(
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.all(16),
+              color: Colors.red,
+              child: const Center(
+                child: Text(
+                  "Segundo container",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
